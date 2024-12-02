2232dddd4c4ceab078d7626ca38a6d784387ee2c
rex <rex@host-kali>
2024-12-03 02:32:01 +0530

    Removed Old posts and added new post with some minor changes

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin-privilege/", admin.site.urls),
     path("",include("portfolio_app.urls")),
     path("blogs/",include("myapp.urls"))
 ]
